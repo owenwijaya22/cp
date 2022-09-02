14e6cc91ace0033c221db33d11d6f1460c2b1bba
Owen Valentinus <owenwijaya89@gmail.com>
2022-09-02 09:57:09 +0800

    added sentences

--- ntu/2.py
+++ ntu/2.py
@@ -1,2 +1,2 @@
 integers = list(map(int, input('Input in one line, e.g. 1 2 3: ').split()))
-print(max(integers)-min(integers))
+print(f"The numerical range between these three integers are: {max(integers)-min(integers)}")
